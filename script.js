10566b0fb4dac6f989136c0241d2d80e4afb80d8
Matt Wang <mattwang1997@gmail.com>
2017-01-31 01:02:49 -0500

    Added menu-remove

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function dropMenu() {
     document.getElementById("dropdown").classList.toggle("show");
-    // document.getElementById("page-content").classList.toggle("menuvis");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
 }
